5728a2526568a13e2e5e5b803abb9505f984c23f
Mikhail Utin <utinspb3@yandex.ru>
2022-07-14 17:15:17 +0300

    ref: add text mixins

--- src/components/elements/Title.tsx
+++ src/components/elements/Title.tsx
@@ -1,14 +1,11 @@
 import { FC } from "react";
 
 interface TitlePropsType {
-  children: string | React.ReactNode
+  children: string | React.ReactNode;
+  className?: string;
 }
 
-const Title: FC<TitlePropsType> = ({children}) => {
-  return (
-    <>
-      <h1 className="title">{children}</h1>
-    </>
-  )
-}
+const Title: FC<TitlePropsType> = ({ children, ...props }) => {
+  return <h1 className={`title ${props.className}`}>{children}</h1>;
+};
 export default Title;
